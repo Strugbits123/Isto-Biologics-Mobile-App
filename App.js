428ff073ebf4734fd28d79b696ffd42afefd3e57
janisar-strugbits <janisar@strugbitsglobal.com>
2024-10-31 22:14:42 +0500

    "Hello world build"

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import "./polyfills";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import * as React from "react";
+import { Text, View } from "react-native";
 import "react-native-gesture-handler";
 import "react-native-url-polyfill/auto";
 import { WixSessionProvider } from "./authentication/session";
@@ -8,10 +9,24 @@ import { LoginHandler } from "./authentication/LoginHandler";
 import App_Navigation from "./config/App_Navigation";
 import { PointsProvider } from "./components/PointsHandler";
 import { CurrentMemberProvider } from "./components/CurrentMemberHandler";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  PlusJakartaSans_700Bold,
+  useFonts,
+} from "@expo-google-fonts/plus-jakarta-sans";
 
+// Prevent auto-hiding of the splash screen
+SplashScreen.preventAutoHideAsync();
 const queryClient = new QueryClient();
 
 function App() {
+  const [fontsLoaded, fontsError] = useFonts({ PlusJakartaSans_700Bold });
+
+  React.useEffect(() => {
+    if (fontsLoaded || fontsError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontsError]);
   return (
     <>
       <QueryClientProvider client={queryClient}>
@@ -19,7 +34,23 @@ function App() {
           <LoginHandler>
             <CurrentMemberProvider>
               <PointsProvider>
-                <App_Navigation />
+                {/* <App_Navigation /> */}
+                <View
+                  style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: 18,
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 18,
+                    }}
+                  >
+                    Hello Janisar
+                  </Text>
+                </View>
               </PointsProvider>
             </CurrentMemberProvider>
           </LoginHandler>
